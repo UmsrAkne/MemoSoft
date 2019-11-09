2454e473bf0dc0ec5c644b2265ce85fe539e0399
Akn <sibata486@gmail.com>
2019-11-09 18:55:46 +0900

    テキスト保存処理を記述するクラスを新規追加

---
+++ MemoSoft/Models/TextSaver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoSoft.Models
+{
+    /// <summary>
+    /// このクラスは入力されたテキストを保存します。
+    /// テキストの入力には、プロパティを使用してください。
+    /// </summary>
+    class TextSaver
+    {
+        public string Text { get; set; }
+
+        public void saveText() {
+        }
+    }
+}
