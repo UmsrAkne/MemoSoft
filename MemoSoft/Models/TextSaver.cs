2724f07d3622071a160e44ceb4cd7186d91807ca
Akn <sibata486@gmail.com>
2019-12-27 20:11:49 +0900

    テキストをデータベースに記録する機能を追加

--- MemoSoft/Models/TextSaver.cs
+++ MemoSoft/Models/TextSaver.cs
@@ -12,9 +12,29 @@ namespace MemoSoft.Models
     /// </summary>
     class TextSaver
     {
+        private DatabaseHelper dbHelper;
+
+        private readonly String DATABASE_NAME = "Diarydb";
+        private readonly String DATABASE_TABLE_NAME = "diary";
+        private readonly String DATABASE_COLUMN_NAME_DATE = "date";
+        private readonly String DATABASE_COLUMN_NAME_TEXT = "text";
+
+        public TextSaver() {
+            this.dbHelper = new DatabaseHelper(DATABASE_NAME);
+            this.dbHelper.createDatabase();
+
+            this.dbHelper.createTable(DATABASE_TABLE_NAME,
+                new String[] { DATABASE_COLUMN_NAME_DATE, DATABASE_COLUMN_NAME_TEXT }
+                );
+        }
+
         public string Text { get; set; }
 
         public void saveText() {
+            dbHelper.insertData(
+                DATABASE_TABLE_NAME,
+                new String[] { DATABASE_COLUMN_NAME_DATE, DATABASE_COLUMN_NAME_TEXT },
+                new String[] {DateTime.Now.ToString("yyyyMMddHHmmssff"), Text });
         }
     }
 }
