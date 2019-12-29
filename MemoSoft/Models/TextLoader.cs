b25d6424006a70b6691d3c2c4b9fa6b968c84c8a
Akn <sibata486@gmail.com>
2019-12-30 00:34:53 +0900

    記録済みのテキストを読み込むモデルクラスを追加

---
+++ MemoSoft/Models/TextLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoSoft.Models
+{
+    class TextLoader
+    {
+        private DatabaseHelper dbHelper = new DatabaseHelper(DatabaseHelper.DATABASE_NAME);
+
+
+    }
+}
