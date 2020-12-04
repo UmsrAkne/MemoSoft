194b620300bfd3013a9a1c683ccf9196cb373bff
Akn <sibata486@gmail.com>
2020-12-04 20:14:50 +0900

    クラス追加 / Npgsql を扱う機能を備えたクラスを追加

---
+++ MemoSoft/Models/NpgsqlExecuter.cs
@@ -0,0 +1,67 @@
+﻿using Npgsql;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoSoft.Models {
+    public class NpgsqlExecuter{
+
+        public NpgsqlExecuter() {
+
+            string basePath = 
+                Environment.GetEnvironmentVariable("HOMEDRIVE") +
+                Environment.GetEnvironmentVariable("HOMEPATH")  + @"\awsrds\" ;
+
+            string readText(string path) {
+                using (var sr = new StreamReader(path)) {
+                    return sr.ReadToEnd();
+                }
+            }
+
+            ConnectionBuilder = new NpgsqlConnectionStringBuilder() {
+                Host = readText(basePath + "hostName.txt" ),
+                Username = readText(basePath + "user.txt"),
+                Password = readText(basePath + "pass.txt"),
+                Port = int.Parse(readText(basePath + "port.txt")),
+                Database = "postgres"
+            };
+        }
+
+        public void executeNonQuery(string sql, List<NpgsqlParameter> parameters) {
+            using (var conn = new NpgsqlConnection()) {
+                conn.Open();
+                var command = new NpgsqlCommand(sql, new NpgsqlConnection());
+                parameters.ForEach(p => command.Parameters.Add(p));
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public List<Hashtable> select(string sql, List<NpgsqlParameter> parameters) {
+            using (var con = Connection) {
+                List<Hashtable> resultList = new List<Hashtable>();
+                con.Open();
+                var command = new NpgsqlCommand(sql, con);
+                parameters.ForEach(p => command.Parameters.Add(p));
+                var dataReader = command.ExecuteReader();
+
+                while (dataReader.Read()) {
+                    var hashtable = new Hashtable();
+                    for (int i = 0; i < dataReader.FieldCount; i++) {
+                        hashtable[dataReader.GetName(i)] = dataReader.GetValue(i);
+                    }
+                    resultList.Add(hashtable);
+                }
+
+                return resultList;
+            };
+        }
+
+        private NpgsqlConnectionStringBuilder ConnectionBuilder { get; set; }
+        private NpgsqlConnection Connection { get; set; }
+    }
+}
