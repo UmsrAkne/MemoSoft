34b1adbe8028ef7821f36b7cf30d9ab7f6815b00
Akn <sibata486@gmail.com>
2020-12-04 19:19:41 +0900

    パッケージ Prism.Wpf を追加
    パッケージなしでやると無駄な手間がかかりすぎるため必須

--- MemoSoft/ViewModels/MainWindowViewModel.cs
+++ MemoSoft/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MemoSoft.ViewModels {
-    class MainWindowViewModel {
+    class MainWindowViewModel : BindableBase{
 
     }
 }
