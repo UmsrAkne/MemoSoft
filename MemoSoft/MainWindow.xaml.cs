3b8b4d7a507c2e7fb3312171870b69f618017d20
Akn <sibata486@gmail.com>
2021-07-10 12:29:07 +0900

    add / MainWindow.xaml.cs にウィンドウの位置、サイズを復元する機能を実装

--- MemoSoft/MainWindow.xaml.cs
+++ MemoSoft/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using MemoSoft.Models;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Collections.ObjectModel;
+using MemoSoft.Properties;
 
 namespace MemoSoft
 {
@@ -30,13 +31,70 @@ namespace MemoSoft
         public MainWindow() {
             InitializeComponent();
             this.mainWindowViewModel = new MainWindowViewModel();
+            RecoverWindowBounds();
         }
 
         private void App_Activated(object sender, EventArgs e) {
             Keyboard.Focus(this.textBox);
         }
-    }
 
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            // ウィンドウのサイズを保存
+            SaveWindowBounds();
+            base.OnClosing(e);
+        }
+
+        /// <summary>
+        /// ウィンドウの位置・サイズを保存します。
+        /// </summary>
+        private void SaveWindowBounds()
+        {
+            var settings = Settings.Default;
+            settings.WindowMaximized = WindowState == WindowState.Maximized;
+            WindowState = WindowState.Normal; // 最大化解除
+            settings.WindowLeft = Left;
+            settings.WindowTop = Top;
+            settings.WindowWidth = Width;
+            settings.WindowHeight = Height;
+            settings.Save();
+        }
+
+        /// <summary>
+        /// ウィンドウの位置・サイズを復元します。
+        /// </summary>
+        private void RecoverWindowBounds()
+        {
+            var settings = Settings.Default;
+
+            // 左
+            if (settings.WindowLeft >= 0 &&
+                (settings.WindowLeft + settings.WindowWidth) < SystemParameters.VirtualScreenWidth)
+            { Left = settings.WindowLeft; }
+
+            // 上
+            if (settings.WindowTop >= 0 &&
+                (settings.WindowTop + settings.WindowHeight) < SystemParameters.VirtualScreenHeight)
+            { Top = settings.WindowTop; }
+
+            // 幅
+            if (settings.WindowWidth > 0 &&
+                settings.WindowWidth <= SystemParameters.WorkArea.Width)
+            { Width = settings.WindowWidth; }
+
+            // 高さ
+            if (settings.WindowHeight > 0 &&
+                settings.WindowHeight <= SystemParameters.WorkArea.Height)
+            { Height = settings.WindowHeight; }
+
+            // 最大化
+            if (settings.WindowMaximized)
+            {
+                // ロード後に最大化
+                Loaded += (o, e) => WindowState = WindowState.Maximized;
+            }
+        }
+    }
 
     public class MainWindowViewModel : INotifyPropertyChangedBase{
         private string inputString;
